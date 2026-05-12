1acd4834c7c5e51e1d796ed5a6a202075fc574bb
TrueCrimeDev <truecrime.audit@gmail.com>
2026-05-12 17:25:38 -0400

    Make Script::ParseExprToPostfix public so BIF_Eval can call it

--- source/script.h
+++ source/script.h
@@ -2236,6 +2236,7 @@ private:
 	ResultType AddLabel(LPTSTR aLabelName, bool aAllowDupe);
 	ResultType AddLine(ActionTypeType aActionType, LPTSTR aArg[] = NULL, int aArgc = 0, bool aAllArgsAreExpressions = false);
 
+public:
 	// Parse a single AHK expression string into a heap-allocated scratch Line whose
 	// mArg[0].postfix is ready for Line::ExpandExpression. Variable identifiers
 	// resolve against aResolveScope first, then globals. The Line is NOT linked
@@ -2243,6 +2244,7 @@ private:
 	// Returns OK on success; FAIL on parse failure (error propagates as AHK exception).
 	ResultType ParseExprToPostfix(LPTSTR aExpr, UserFunc *aResolveScope,
 	                              Line *&aOutLine, LPTSTR &aErrMsg, int &aErrColumn);
+private:
 
 	ResultType PreparseExpressions();
 	ResultType PreparseExpressions(Line *aStartingLine);
